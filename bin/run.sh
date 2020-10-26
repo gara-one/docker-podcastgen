664c4e4f8acd2ba557246618f119fd983a20ea50
gara-one <73467307+gara-one@users.noreply.github.com>
2020-10-26 14:22:34 +0000

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -20,7 +20,7 @@ if [[ ! -d $DIR ]]; then
 	mv /tmp/PodcastGenerator-${PODCASTGEN_VERSION} ${DIR}
 	[[ -d $DIR ]] || { ERR "Directory $DIR does not exist, aborting."; exit 1; }
 	chown -R www-data:www-data /var/www/
-	sed -i -e "s#^php-docroot\ *=.*#php-docroot\ =\ ${DIR}#" \
+	sed -i -e "s#^php-docroot\ *=\ {{\ PODCASTGEN_DIR\ }}#php-docroot\ =\ ${DIR}#" \
 		-e "s#^static-safe\ *=\ {{\ PODCASTGEN_DIR\ }}#static-safe\ =\ ${DIR}#" \
 		 -e "s#^php-set\ =\ post_max_size=100M#php-set\ =\ post_max_size=${PHP_SIZE}#" \
 		  -e "s#^php-set\ =\ upload_max_filesize=100M#php-set\ =\ upload_max_filesize=${PHP_SIZE}#" \		  
